ee7060952bacff5ce8bec04ac63b4cd1f3b725fb
Jake <panamajake86@gmail.com>
2019-09-27 21:32:34 -0600

    I ran a shit ton of api pulls.

--- starwars/src/components/SWCard.js
+++ starwars/src/components/SWCard.js
@@ -1,16 +1,45 @@
 import React from "react";
-import { Card, CardText, CardBody, CardTitle, CardImg, Col } from "reactstrap";
+import { Card, CardText, CardBody, CardTitle, Col } from "reactstrap";
+import Axios from "axios";
 
 export default function SWCard(props) {
+
+
+    const [speciesName, getSpeciesName] = React.useState();
+    const [homeName, getHomeName] = React.useState();
+    const [films, getFilms] = React.useState([]);
+    const [ship, getShip] = React.useState([]);
+
+    React.useEffect(() => {
+        async function getSWCardInfo() {
+            try {
+                const species = await Axios.get(props.species);
+                getSpeciesName(species.data.name)
+                const home = await Axios.get(props.homeworld);
+                getHomeName(home.data.name)
+                const film = await Axios.get(props.films[0]);
+                getFilms(film.data.title)
+                const ships = await Axios.get(props.starships[0]);
+                getShip(ships.data.name)
+
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
+        getSWCardInfo();
+    }, [])
+
+
 
     return (
         <Col height="100%">
             <Card height="100%">
                 <CardBody height="100%">
                     <CardTitle>{props.name}</CardTitle>
-                    <CardText>{props.species} from {props.homeworld}</CardText>
-                    <CardText>Appears in {props.films}</CardText>
-                    <CardText>Pilots {props.starships} {props.vehicles}</CardText>
+                    <CardText><p>{speciesName} from {homeName}</p></CardText>
+                    <CardText>First appears in {films}</CardText>
+                    <CardText>Pilots {ship}</CardText>
                 </CardBody>
             </Card>
         </Col>
